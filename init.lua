e23b27af219272eb12982f098aac57dd4a1ba7e4
YouheiKajiro <te0801068@gmail.com>
2022-08-23 21:29:34 +0900

    stylua.toml追加

--- init.lua
+++ init.lua
@@ -21,90 +21,90 @@ vim.api.nvim_set_keymap("n", "<c-j>", "<c-w><c-j>", { noremap = true, silent = t
 vim.api.nvim_set_keymap("n", "<c-l>", "<c-w><c-l>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>g",
-	"<cmd>lua require('fzf-lua').git_files()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<leader>g",
+  "<cmd>lua require('fzf-lua').git_files()<CR>",
+  { noremap = true, silent = true }
 )
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>r",
-	"<cmd>lua require('fzf-lua').grep_project()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<leader>r",
+  "<cmd>lua require('fzf-lua').grep_project()<CR>",
+  { noremap = true, silent = true }
 )
 
 local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
 end
 
 require("lspconfig")["tsserver"].setup({
-	on_attach = on_attach,
-	flags = lsp_flags,
+  on_attach = on_attach,
+  flags = lsp_flags,
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers({
-	function(server_name) -- default handler (optional)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-		})
-	end,
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup({
+      on_attach = on_attach,
+    })
+  end,
 })
 
 local cmp = require("cmp")
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("snippy").expand_snippet(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "snippy" },
-	}, {
-		{ name = "buffer" },
-	}),
+  snippet = {
+    expand = function(args)
+      require("snippy").expand_snippet(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "snippy" },
+  }, {
+    { name = "buffer" },
+  }),
 })
 
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "path" },
-		{ name = "cmdline" },
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "path" },
+    { name = "cmdline" },
+  },
 })
 
 require("snippy").setup({
-	mappings = {
-		is = {
-			["<Tab>"] = "expand_or_advance",
-			["<S-Tab>"] = "previous",
-		},
-	},
+  mappings = {
+    is = {
+      ["<Tab>"] = "expand_or_advance",
+      ["<S-Tab>"] = "previous",
+    },
+  },
 })
 
 require("nvim_comment").setup()
@@ -113,12 +113,12 @@ require("auto-save").setup({})
 require("nvim-autopairs").setup({})
 require("gitsigns").setup()
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "lua", "gomod", "markdown", "sql", " typescript", "vue", "json", "javascript" },
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-	},
+  ensure_installed = { "go", "lua", "gomod", "markdown", "sql", " typescript", "vue", "json", "javascript" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
 })
 
 local actions = require("lir.actions")
@@ -126,116 +126,116 @@ local mark_actions = require("lir.mark.actions")
 local clipboard_actions = require("lir.clipboard.actions")
 
 require("lir").setup({
-	show_hidden_files = false,
-	devicons_enable = true,
-	mappings = {
-		["l"] = actions.edit,
-		["<C-s>"] = actions.split,
-		["<C-v>"] = actions.vsplit,
-		["<C-t>"] = actions.tabedit,
+  show_hidden_files = false,
+  devicons_enable = true,
+  mappings = {
+    ["l"] = actions.edit,
+    ["<C-s>"] = actions.split,
+    ["<C-v>"] = actions.vsplit,
+    ["<C-t>"] = actions.tabedit,
 
-		["h"] = actions.up,
-		["q"] = actions.quit,
+    ["h"] = actions.up,
+    ["q"] = actions.quit,
 
-		["K"] = actions.mkdir,
-		["N"] = actions.newfile,
-		["R"] = actions.rename,
-		["@"] = actions.cd,
-		["Y"] = actions.yank_path,
-		["."] = actions.toggle_show_hidden,
-		["D"] = actions.delete,
+    ["K"] = actions.mkdir,
+    ["N"] = actions.newfile,
+    ["R"] = actions.rename,
+    ["@"] = actions.cd,
+    ["Y"] = actions.yank_path,
+    ["."] = actions.toggle_show_hidden,
+    ["D"] = actions.delete,
 
-		["J"] = function()
-			mark_actions.toggle_mark()
-			vim.cmd("normal! j")
-		end,
-		["C"] = clipboard_actions.copy,
-		["X"] = clipboard_actions.cut,
-		["P"] = clipboard_actions.paste,
-	},
-	float = {
-		winblend = 0,
-		curdir_window = {
-			enable = false,
-			highlight_dirname = false,
-		},
+    ["J"] = function()
+      mark_actions.toggle_mark()
+      vim.cmd("normal! j")
+    end,
+    ["C"] = clipboard_actions.copy,
+    ["X"] = clipboard_actions.cut,
+    ["P"] = clipboard_actions.paste,
+  },
+  float = {
+    winblend = 0,
+    curdir_window = {
+      enable = false,
+      highlight_dirname = false,
+    },
 
-		-- -- You can define a function that returns a table to be passed as the third
-		-- -- argument of nvim_open_win().
-		-- win_opts = function()
-		--   local width = math.floor(vim.o.columns * 0.8)
-		--   local height = math.floor(vim.o.lines * 0.8)
-		--   return {
-		--     border = {
-		--       "+", "─", "+", "│", "+", "─", "+", "│",
-		--     },
-		--     width = width,
-		--     height = height,
-		--     row = 1,
-		--     col = math.floor((vim.o.columns - width) / 2),
-		--   }
-		-- end,
-	},
-	hide_cursor = true,
-	on_init = function()
-		-- use visual mode
-		vim.api.nvim_buf_set_keymap(
-			0,
-			"x",
-			"J",
-			':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
-			{ noremap = true, silent = true }
-		)
+    -- -- You can define a function that returns a table to be passed as the third
+    -- -- argument of nvim_open_win().
+    -- win_opts = function()
+    --   local width = math.floor(vim.o.columns * 0.8)
+    --   local height = math.floor(vim.o.lines * 0.8)
+    --   return {
+    --     border = {
+    --       "+", "─", "+", "│", "+", "─", "+", "│",
+    --     },
+    --     width = width,
+    --     height = height,
+    --     row = 1,
+    --     col = math.floor((vim.o.columns - width) / 2),
+    --   }
+    -- end,
+  },
+  hide_cursor = true,
+  on_init = function()
+    -- use visual mode
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "x",
+      "J",
+      ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+      { noremap = true, silent = true }
+    )
 
-		-- echo cwd
-		vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
-	end,
+    -- echo cwd
+    vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
+  end,
 })
 vim.api.nvim_set_keymap("n", "<C-n>", '<cmd>lua require("lir.float").toggle()<cr>', { noremap = true })
 
 -- custom folder icon
 require("nvim-web-devicons").set_icon({
-	lir_folder_icon = {
-		icon = "",
-		color = "#7ebae4",
-		name = "LirFolderNode",
-	},
+  lir_folder_icon = {
+    icon = "",
+    color = "#7ebae4",
+    name = "LirFolderNode",
+  },
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	on_attach = function(client, bufnr)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
 
-			-- format on save
-			vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
-		end
+      -- format on save
+      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+    end
 
-		if client.server_capabilities.documentRangeFormattingProvider then
-			vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
-		end
-	end,
+    if client.server_capabilities.documentRangeFormattingProvider then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
+  end,
 })
 local prettier = require("prettier")
 
 prettier.setup({
-	bin = "prettierd", -- or `prettierd`
-	filetypes = {
-		"css",
-		"graphql",
-		"html",
-		"javascript",
-		"javascriptreact",
-		"json",
-		"less",
-		"markdown",
-		"scss",
-		"typescript",
-		"typescriptreact",
-		"yaml",
-	},
+  bin = "prettierd", -- or `prettierd`
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
 })
 
 vim.opt.completeopt = "menu,menuone,noselect"
